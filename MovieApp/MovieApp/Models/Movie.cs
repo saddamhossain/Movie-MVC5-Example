b5ec58b02b8e55ce1990ee94b152126da1d22139
realsteel <engr.rana.csc@gmail.com>
2014-11-15 17:35:38 +0600

    first commit

---
+++ MovieApp/MovieApp/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public DateTime RelaseDate { set; get; }
+
+
+    }
+}
